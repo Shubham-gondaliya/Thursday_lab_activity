f9b3651800450efde134f047594f16ebf901aa4e
Shubham-gondaliya <109596173+Shubham-gondaliya@users.noreply.github.com>
2022-07-28 17:38:16 +0530

    Update cube.java

--- cube.java
+++ cube.java
@@ -1,20 +1,20 @@
 //Write a  Java program  to display the cube of the number upto given  integer.
 
 import java.util.Scanner;
- class cube {
+ class cube {                                 //main class
 
    public static void main(String[] args)
 
 {
     int i,no,cube;
     System.out.println("Enter a No :- ");
-    Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);     //Taking the value from the user
 		    no = sc.nextInt();
 
-     for(i=1;i<=no;i++)
+     for(i=1;i<=no;i++)                       //Moving a loop
      {
          
-         cube = i*i*i;
+         cube = i*i*i;                		//syntax of cube
          System.out.println("no is " +i+ " cube of no :-" +i+" is "  +cube );
          // System.out.println("\n");
 
