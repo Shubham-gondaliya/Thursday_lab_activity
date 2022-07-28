ec9972d525ebe5f5b9694fc9d63e0575000c9cf9
Shubham-gondaliya <109596173+Shubham-gondaliya@users.noreply.github.com>
2022-07-28 17:39:28 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,17 +1,17 @@
 //Write a Java program to display the pattern like right angle triangle with a number. 
 
 import java.util.Scanner;
-class pattern
+class pattern 						//main class
 {
 	public static void main(String[] args) {
 		int i,j,number;
 		System.out.println("enter number :- ");
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);	//Taking the value from the user
 		number = sc.nextInt();
 
-		for(i=1;i<=number;i++)
+		for(i=1;i<=number;i++) 			//moving a loop for row
 		{
-			for (j=1;j<=i;j++) 
+			for (j=1;j<=i;j++) 		//moving a loop for column
 			{
 				System.out.println(j);
 			}
