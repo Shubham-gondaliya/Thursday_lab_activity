2ab11743712fe0f2a479ab778c829affb1a71695
Shubham-gondaliya <109596173+Shubham-gondaliya@users.noreply.github.com>
2022-08-04 17:14:06 +0530

    Update clockwisearray.java

--- clockwisearray.java
+++ clockwisearray.java
@@ -4,14 +4,14 @@ class clockwisearray
 {
 	public static void main(String[] args) {
 		int [] a = {10,20,30,40,50,60};
-
-		for(int i=0;i<a.length;i++)
+		System.out.println("original element is :- ")
+		for(int i=0;i<a.length;i++)  //using for loop
 		{
-			System.out.println(+a[i]);
+			System.out.println(+a[i]);  
 		}
 		System.out.println("reverse element is :- ");
 	
-		 for(int i=a.length-1;i>=0;i--)
+		 for(int i=a.length-1;i>=0;i--)		//using for loop for reverse element.
 		 {
 		 	System.out.println(+a[i]);
 		 }
