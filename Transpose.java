c742a84d77749a0d551e7d5c962dc29225cc621d
Shubham-gondaliya <109596173+Shubham-gondaliya@users.noreply.github.com>
2022-08-04 17:13:05 +0530

    Update Transpose.java

--- Transpose.java
+++ Transpose.java
@@ -1,3 +1,5 @@
+//Write a java program to find transpose of a given matrix.
+
 import java.util.Scanner;
  class Transpose
 {
@@ -6,23 +8,23 @@ import java.util.Scanner;
 	int i, j;
 	System.out.println("Enter total rows and columns: ");
 	Scanner sc = new Scanner(System.in);
-	int row = sc.nextInt();
+	int row = sc.nextInt();                       //get row and col value from user.
 	int column = sc.nextInt();
-	int array[][] = new int[row][column];
+	int array[][] = new int[row][column];   //create two dimenstion array.
 	System.out.println("Enter matrix:");
- 	for(i = 0; i < row; i++)
+ 	for(i = 0; i < row; i++)              //using for loop.
   	{
-   	    for(j = 0; j < column; j++) 
+   	    for(j = 0; j < column; j++)   
      	    {
-        	array[i][j] = sc.nextInt();
+        	array[i][j] = sc.nextInt();        //get value from user.
             }
   	}
  	System.out.println("Transpose is :- ");
-  	for(i = 0; i < column; i++)
+  	for(i = 0; i < column; i++)                     //using loop for transpose
     	{
-      	    for(j = 0; j < row; j++)
+      	    for(j = 0; j < row; j++)             //using loop for transpose
             {
-                System.out.print(+array[j][i]);
+                System.out.print(+array[j][i]);  
             }
             System.out.println(" ");
         }
