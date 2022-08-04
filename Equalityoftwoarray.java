b4be9bc996b2b349d7583e24e8d8ad34db1c96c6
Shubham-gondaliya <109596173+Shubham-gondaliya@users.noreply.github.com>
2022-08-04 17:13:39 +0530

    Update Equalityoftwoarray.java

--- Equalityoftwoarray.java
+++ Equalityoftwoarray.java
@@ -7,7 +7,7 @@ class Equalityoftwoarray
 		{
 			int [] arr1 = {10,20,30,40,80};
 			int [] arr2 = {10,20,30,40,80};
-			System.out.println("Equality of two array is :- "
+			System.out.println("Equality of two array is :- "	//use equal method.
 				+Arrays.equals(arr1, arr2));
  	    }
 }
